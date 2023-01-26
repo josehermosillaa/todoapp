9ef49188ddfd40d9dd88f3e76fd28805076a4fea
josehermosilla <jose.hermosillaa@usach.cl>
2023-01-26 10:03:28 -0300

    add logic in todo.store

--- src/store/todo.store.js
+++ src/store/todo.store.js
@@ -20,6 +20,50 @@ const initStore = () => {
     console.log('InitStore')
 }
 
+const loadStore = () => {
+    throw new Error('Not implemented');
+}
+
+/**
+ * 
+ * @param {String} description 
+ */
+const addTodo = (description) => {
+    throw new Error('Not implemented');
+
+}
+/**
+ * 
+ * @param {String} todoId 
+ */
+const toggleTodo = (todoId) => {
+    throw new Error('Not implemented');
+}
+
+const deleteTodo = (todoID) => {
+    throw new Error('Not implemented');
+
+}
+const deleteCompleted = () => {
+    throw new Error('Not implemented');
+
+}
+
+const setFilter = (newFilter = Filters.All) => {
+    throw new Error('Not implemented');
+}
+
+const getCurrentFilter = () => {
+    throw new Error('Not implemented');
+
+}
 export default {
+    deleteCompleted,
+    deleteTodo,
+    getCurrentFilter,
     initStore,
+    loadStore,
+    setFilter,
+    toggleTodo,
+
 }
